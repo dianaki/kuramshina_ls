faf894820e8d137ea364963e6488542501fbbdce
dianaki <diana.kuramshina21@gmail.com>
2020-08-23 13:16:46 +0300

    add validation

--- src/admin/components/skillAddLine/skillAddLine.vue
+++ src/admin/components/skillAddLine/skillAddLine.vue
@@ -1,19 +1,30 @@
 <template>
-  <div 
-    :class="['skill-add-line-component', {blocked: blocked}]"
-    >
-    <div class="add-title">
-      <app-input 
-      placeholder="Новый навык" 
-      />
-    </div>
-    <div class="add-percent">
-      <app-input type="number" min="0" max="100" maxlength="3" />
-    </div>
-    <div class="add-button">
-      <round-button type="round" />
-    </div>
-  </div> 
+  <div class="">
+    <div 
+      @submit.prevent="someAction()"
+      :class="['skill-add-line-component', {blocked: blocked}]"
+      >
+      <div class="add-title">
+        <app-input 
+        v-model="skill"
+        placeholder="Новый навык" 
+        />
+      </div>
+      <div class="add-percent">
+        <app-input 
+        v-model="percent"
+        type="number" min="0" max="100" maxlength="3" />
+      </div>
+      <div class="add-button">
+        <round-button 
+        :disabled="!isSkillValid"
+        type="round" />
+      </div>
+    </div> 
+    <div 
+    v-if="isSkillError"
+    class="text-error">Не все поля формы заполнены</div>
+  </div>
 </template>
 
 <script>
@@ -27,8 +38,32 @@ export default {
   components: {
     appInput: input,
     roundButton: button,
-  }
-}
+  },
+  data() {
+    return {
+      skill: null,
+      percent: null,
+    };
+  },
+  computed: {
+    isSkillValid() {
+      return this.skill && this.percent;
+    },
+    isSkillError() {
+      return !this.isSkillValid
+    },
+  },
+};
 </script>
 
-<style lang="postcss" src="./skillAddLine.pcss"></style>
+<style lang="postcss" src="./skillAddLine.pcss">
+</style>
+
+<style lang="postcss" scoped>
+
+.text-error {
+  text-align: center;
+  color:red;
+}
+
+</style>
